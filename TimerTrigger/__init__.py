1c2c84847d9af21d21ab70f52caaaba67f4ee933
Kenny Baskett <kbaskett248@gmail.com>
2021-12-11 12:28:23 -0500

    Use common code to handle TimerTrigger

--- TimerTrigger/__init__.py
+++ TimerTrigger/__init__.py
@@ -2,13 +2,26 @@ import datetime
 import logging
 
 import azure.functions as func
+from HttpTrigger import MESSAGE_TEMPLATE
+
+from shared import Source, process_trigger
+
+
+MESSAGE_TEMPLATE = "Processed timer trigger at {current_time}"
 
 
 def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().replace(
-        tzinfo=datetime.timezone.utc).isoformat()
+    utc_timestamp = (
+        datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
+    )
 
+    kwargs = {
+        "current_time": utc_timestamp,
+        "past_due": mytimer.past_due,
+    }
+
+    template = MESSAGE_TEMPLATE
     if mytimer.past_due:
-        logging.info('The timer is past due!')
+        template += "; It is past due."
 
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+    process_trigger(Source.TIMER, template, **kwargs)
