415d168cea74f7ec99cd93eef6cb1d73f2f4c4e2
Kenny Baskett <kbaskett248@gmail.com>
2021-12-11 12:32:51 -0500

    Doc for process_trigger

--- shared/__init__.py
+++ shared/__init__.py
@@ -3,11 +3,25 @@ import logging
 
 
 class Source(Enum):
+    """Simple enum to define the source that we're triggering."""
+
     TIMER = "TIMER"
     HTTP = "HTTP"
 
 
 def process_trigger(source: Source, message_template: str, *args, **kwargs) -> str:
+    """Process a trigger from various sources.
+
+    Args:
+        source (Source): The source of the trigger being processed.
+        message_template (str): A format-compatible template string to log and return.
+            The string is formatted using args and kwargs.
+        *args: Arguments used to format message_template
+        **kwargs: Keyword arguments used to format message_template
+
+    Returns:
+        str: The formatted message template
+    """
     message = f"{source}: {message_template}".format(*args, **kwargs)
     logging.info(message)
     return message
